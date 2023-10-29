945b0b4884579d90381644eff2d6d261b86465a3
Marko Žagar <marko.zagar@fer.hr>
2023-10-29 15:40:36 +0100

    Adding action listeners to form elements in register form (3)

--- android-app/app/src/main/java/progi/imateacup/nestaliljubimci/ui/authentication/RegisterFragment.kt
+++ android-app/app/src/main/java/progi/imateacup/nestaliljubimci/ui/authentication/RegisterFragment.kt
@@ -5,56 +5,174 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.addTextChangedListener
 import progi.imateacup.nestaliljubimci.R
+import progi.imateacup.nestaliljubimci.databinding.FragmentRegisterBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [RegisterFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class RegisterFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    companion object {
+        const val EMAIL_REGEX = "^[\\w-]+@([\\w-]+\\.)+[\\w-]{2,4}\$"
+        const val PASSWORD_REGEX = "^.{6,}$"
+    }
+
+    private var _binding: FragmentRegisterBinding? = null
+    private val binding get() = _binding!!
+
+    //private val viewModel by viewModels<RegisterViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false)
+    ): View {
+        _binding = FragmentRegisterBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment RegisterFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            RegisterFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.shelterNameField.visibility = View.GONE
+
+        super.onViewCreated(view, savedInstanceState)
+        //setOnRegistrationResultAction()
+        initListeners()
+    }
+
+    /*private fun setOnRegistrationResultAction() {
+        viewModel.registrationResultLiveData.observe(viewLifecycleOwner) { isRegistrationSuccessful ->
+            if (isRegistrationSuccessful) {
+                val direction = RegisterFragmentDirections.actionRegisterFragmentToLoginFragment(true)
+                findNavController().navigate(direction)
+            } else {
+                Snackbar.make(binding.root, R.string.registration_unsuccessful, Snackbar.LENGTH_SHORT).show()
+            }
+        }
+    }*/
+
+    private fun initListeners() {
+        with(binding) {
+
+            emailField.setOnFocusChangeListener { _, hasFocus ->
+                if (!hasFocus) {
+                    updateEmailField()
                 }
             }
+
+            passwordFieldLayout.setErrorIconOnClickListener {
+                passwordFieldLayout.error = null
+            }
+
+            passwordField.setOnFocusChangeListener { _, hasFocus ->
+                if (!hasFocus) {
+                    updatePasswordField()
+                }
+            }
+
+            /* clicking the login button does not make the password field lose focus
+            so this method should be present to update the password field if the user instead presses done
+            passwordField.setOnEditorActionListener { _, actionId, _ ->
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    updatePasswordField()
+                }
+                false
+            }*/
+
+            repeatPasswordField.setOnFocusChangeListener { _, hasFocus ->
+                if (!hasFocus) {
+                    updatePasswordsFields()
+                }
+            }
+
+            /*repeatPasswordField.setOnEditorActionListener { _, actionId, _ ->
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    updatePasswordsFields()
+                }
+                false
+            }*/
+
+            emailField.addTextChangedListener {
+                registerButton.isEnabled = validateCredentials()
+            }
+            passwordField.addTextChangedListener {
+                registerButton.isEnabled = validateCredentials()
+            }
+            repeatPasswordField.addTextChangedListener {
+                registerButton.isEnabled = validateCredentials()
+            }
+
+            registerButton.setOnClickListener {
+
+                //viewModel.registerUser(emailField.text.toString().trim(), passwordField.text.toString().trim())
+            }
+
+            registerAsShelterCheckBox.setOnCheckedChangeListener { _, isChecked ->
+                if (isChecked) {
+                    shelterNameField.visibility = View.VISIBLE
+                } else
+                    shelterNameField.visibility = View.GONE
+            }
+        }
     }
+
+    private fun validateEmail(email: String): Boolean {
+        val emailRegex = Regex(EMAIL_REGEX)
+        return email.matches(emailRegex)
+    }
+
+    private fun validatePassword(password: String): Boolean {/*Password must contain at least one digit, one lowercase letter, one uppercase letter, one special character and must be at least 6 characters long*/
+        val passwordRegex = Regex(PASSWORD_REGEX)
+        return password.matches(passwordRegex)
+    }
+
+    private fun checkPasswordsMatch(): Boolean {
+        return binding.passwordField.text.toString().trim() == binding.repeatPasswordField.text.toString().trim()
+    }
+
+    private fun validateCredentials(): Boolean {
+        return validateEmail(binding.emailField.text.toString().trim()) && validatePassword(
+            binding.passwordField.text.toString().trim()
+        ) && checkPasswordsMatch()
+    }
+
+    private fun updateEmailField() {
+        with(binding) {
+            if (!validateEmail(emailField.text.toString().trim())) {
+
+                emailFieldLayout.error = getString(R.string.email_error_message)
+                emailFieldLayout.setErrorTextAppearance(R.style.ErrorTextAppearance)
+
+            } else {
+                emailFieldLayout.error = null
+            }
+        }
+    }
+
+    private fun updatePasswordField() {
+        with(binding) {
+            if (!validatePassword(passwordField.text.toString().trim())) {
+
+                passwordFieldLayout.error = getString(R.string.password_error_message)
+                passwordFieldLayout.setErrorTextAppearance(R.style.ErrorTextAppearance)
+
+            } else {
+                passwordFieldLayout.error = null
+            }
+        }
+    }
+
+    private fun updatePasswordsFields() {
+        with(binding) {
+            if (!checkPasswordsMatch()) {
+                repeatPasswordFieldLayout.error = getString(R.string.passwords_dont_match)
+                repeatPasswordFieldLayout.setErrorTextAppearance(R.style.ErrorTextAppearance)
+                repeatPasswordField.setText("")
+            } else {
+                repeatPasswordFieldLayout.error = null
+            }
+        }
+    }
+
 }
