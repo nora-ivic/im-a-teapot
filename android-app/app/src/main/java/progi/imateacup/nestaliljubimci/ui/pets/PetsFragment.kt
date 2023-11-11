9282b0c68943ed8e3e7ecbccf503b379335e4a49
Bruno Mikan <brunomikan@gmail.com>
2023-11-11 19:08:39 +0100

    Generate fragment files and translate strings.xml

---
+++ android-app/app/src/main/java/progi/imateacup/nestaliljubimci/ui/pets/PetsFragment.kt
@@ -0,0 +1,27 @@
+package progi.imateacup.nestaliljubimci.ui.pets
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import progi.imateacup.nestaliljubimci.R
+
+class PetsFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_pets, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
