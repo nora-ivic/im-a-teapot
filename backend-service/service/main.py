1dd3aa6bafab85110fcc9b475779d2590c711431
mirta <mirtakrajinovic17@gmail.com>
2023-11-13 11:22:09 +0100

    updated validations

--- backend-service/service/main.py
+++ backend-service/service/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Response
+from fastapi.exceptions import RequestValidationError
 
 from migrations import run_migrations
 from service.api.authorization.endpoints import auth_router
@@ -12,3 +13,8 @@ app.include_router(auth_router, prefix="/api/authorization")
 @app.on_event("startup")
 async def startup_event():
     run_migrations(DEFAULT_DATABASE)
+
+
+@app.exception_handler(RequestValidationError)
+def validation_exception_handler(request: Request, exc: RequestValidationError):
+    return Response(status_code=400, content=str(exc))
