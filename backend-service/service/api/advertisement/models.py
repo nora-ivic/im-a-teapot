7ec78f144dd62e0280a28d57b45cd827a1e91dae
mirta <mirtakrajinovic17@gmail.com>
2023-12-09 13:43:52 +0100

    full output advertisement model

--- backend-service/service/api/advertisement/models.py
+++ backend-service/service/api/advertisement/models.py
@@ -1,12 +1,29 @@
+from datetime import datetime
+from typing import List
+
 from pydantic import BaseModel
 
-from service.enums import AdvertisementCategory
+from service.enums import AdvertisementCategory, PetSpecies
 
 
 class AdvertisementOutputShort(BaseModel):
     advert_id: int
     advert_category: AdvertisementCategory
-    pet_name: str = None
+    pet_name: str
     username: str
     picture_link: str = None
 
+
+class AdvertisementOutputFull(BaseModel):
+    advert_id: int
+    advert_category: AdvertisementCategory
+    pet_name: str
+    pet_species: PetSpecies = None
+    pet_color: str = None
+    pet_age: int = None
+    date_time_lost: datetime = None
+    location_lost: str = None
+    description: str = None
+    is_in_shelter: bool = False
+    username: str
+    picture_links: List[str] = []
