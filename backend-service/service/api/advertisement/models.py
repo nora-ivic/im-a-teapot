e9f215ca0140de771ee2882c691c0367336108eb
mirta <mirtakrajinovic17@gmail.com>
2023-12-09 15:38:02 +0100

    endpoint advert details

--- backend-service/service/api/advertisement/models.py
+++ backend-service/service/api/advertisement/models.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from typing import List
+from typing import List, Optional
 
 from pydantic import BaseModel
 
@@ -11,19 +11,19 @@ class AdvertisementOutputShort(BaseModel):
     advert_category: AdvertisementCategory
     pet_name: str
     username: str
-    picture_link: str = None
+    picture_link: Optional[str] = None
 
 
 class AdvertisementOutputFull(BaseModel):
     advert_id: int
     advert_category: AdvertisementCategory
     pet_name: str
-    pet_species: PetSpecies = None
-    pet_color: str = None
-    pet_age: int = None
-    date_time_lost: datetime = None
-    location_lost: str = None
-    description: str = None
+    pet_species: Optional[PetSpecies] = None
+    pet_color: Optional[str] = None
+    pet_age: Optional[int] = None
+    date_time_lost: Optional[datetime] = None
+    location_lost: Optional[str] = None
+    description: Optional[str] = None
     is_in_shelter: bool = False
     username: str
     picture_links: List[str] = []
--- backend-service/service/api/advertisement/utils.py
+++ backend-service/service/api/advertisement/utils.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from service.api.advertisement.models import AdvertisementOutputShort
+from service.api.advertisement.models import AdvertisementOutputShort, AdvertisementOutputFull
 from service.repository.mappers import Advertisement, UserCustom, Pet, Picture
 
 
@@ -15,4 +15,25 @@ def map_to_output_advert_short(db_advert: Advertisement):
         username=user.username,
         pet_name=pet.name if pet.name else '?',
         picture_link=pictures[0].link if pictures[0] else None
+    )
+
+
+def map_to_output_advert_full(db_advert: Advertisement):
+    user: UserCustom = db_advert.user_posted
+    pet: Pet = db_advert.pet_posted
+    pictures: List[Picture] = db_advert.picture_posted
+
+    return AdvertisementOutputFull(
+        advert_id=db_advert.id,
+        advert_category=db_advert.category,
+        pet_name=pet.name if pet.name else '?',
+        pet_species=pet.species if pet.species else None,
+        pet_color=pet.color,
+        pet_age=pet.age,
+        date_time_lost=pet.date_time_lost,
+        location_lost=pet.location_lost,
+        description=pet.description,
+        is_in_shelter=db_advert.is_in_shelter,
+        username=user.username,
+        picture_links=[picture.link for picture in pictures]
     )
