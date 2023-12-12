34604ddd3937804bba96e2b757635a1170b1a9af
Marko Žagar <marko.zagar@fer.hr>
2023-12-12 11:25:04 +0100

    Setting up app to work on local DB, connecting to endpoint, designing advert details fragment, creating image adapter (18)

--- android-app/app/src/main/java/progi/imateacup/nestaliljubimci/model/networking/entities/Shelter.kt
+++
@@ -1,9 +0,0 @@
-package progi.imateacup.nestaliljubimci.model.networking.entities
-
-import kotlinx.serialization.SerialName
-
-@kotlinx.serialization.Serializable
-data class Shelter(
-    @SerialName("id") val id: String,
-    @SerialName("name") val name: String,
-    )
