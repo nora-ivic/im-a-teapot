0224724242c132fc5b5acad6de075e110367f19a
nora-ivic <nora.ivic@fer.hr>
2023-12-26 22:49:41 +0100

    filter1

--- backend-service/service/api/advertisement/endpoints.py
+++ backend-service/service/api/advertisement/endpoints.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, Query, HTTPException
 from typing import Annotated, Optional, List
 
 from service.api.advertisement.models import AdvertisementOutputShort, AdvertisementOutputFull
-from service.api.advertisement.filters import AdvertisementFilter
+from service.api.advertisement.filters import AdvertisementFilter, get_advert_filter
 from service.api.advertisement.utils import map_to_output_advert_short, map_to_output_advert_full
 from service.api.authorization.utils import validate_token
 from service.repository.advertisement_repo import AdvertisementRepository
@@ -13,12 +13,12 @@ advert_router = APIRouter()
 @advert_router.get('/')
 def home_page(
         user_id: Annotated[int, Depends(validate_token)],
-        # filter: Annotated[AdvertisementFilter, Query()] = None,
+        filter_: AdvertisementFilter = Depends(get_advert_filter),
         page: Annotated[int, Query(gt=0, le=100)] = 1,
         page_size: Annotated[int, Query(gt=0, le=100)] = 5
 ) -> List[AdvertisementOutputShort]:
     repo = AdvertisementRepository()
-    db_adverts = repo.get_adverts(user_id, page, page_size)
+    db_adverts = repo.get_adverts(user_id, page, page_size, filter_)
     output_adverts = []
 
     for db_advert in db_adverts:
@@ -36,7 +36,7 @@ def details(
     db_advert = repo.get_advert_by_id(advert_id)
 
     if not db_advert:
-        raise HTTPException(status_code=400, detail='Advert not found!')
+        raise HTTPException(status_code=404, detail='Advert not found!')
 
     output_advert = map_to_output_advert_full(db_advert)
 
--- backend-service/service/repository/advertisement_repo.py
+++ backend-service/service/repository/advertisement_repo.py
@@ -1,6 +1,7 @@
 from sqlalchemy import desc
 from sqlalchemy.orm import Session, joinedload
 from typing import Optional
+from datetime import date
 
 from service.api.advertisement.filters import AdvertisementFilter
 from service.repository.engine_manager import get_session
@@ -12,8 +13,31 @@ class AdvertisementRepository:
     def __init__(self, session: Optional[Session] = None):
         self.session = session if session else get_session()
 
-    def _filter_query(self, query):
-        # TODO implement search
+    def _filter_query(self, query, filter_):
+        if filter_.advert_category:
+            query = query.filter(Advertisement.category == filter_.advert_category.value)
+        if filter_.pet_name:
+            query = query.filter(Advertisement.pet_posted.name.lower() == filter_.pet_name.lower())
+        if filter_.pet_species:
+            query = query.filter(Advertisement.pet_posted.species == filter_.pet_species.value)
+        if filter_.pet_color:
+            query = query.filter(Advertisement.pet_posted.color.lower() == filter_.pet_color.lower())
+        if filter_.pet_age:
+            query = query.filter(Advertisement.pet_posted.age == filter_.pet_age)
+        if filter_.date_time_lost:
+            query = query.filter(Advertisement.pet_posted.date_time_lost.date() == filter_.date_time_lost)
+        if filter_.location_lost:
+            # TODO maybe filter by location
+            pass
+        if filter_.description:
+            query = query.filter(filter_.description.lower() in Advertisement.pet_posted.description.lower())
+        if filter_.is_in_shelter is not None:
+            query = query.filter(Advertisement.is_in_shelter == filter_.is_in_shelter)
+        if filter_.username:
+            query = query.filter(Advertisement.user_posted.username == filter_.username)
+        if filter_.shelter_name:
+            query = query.filter(Advertisement.shelter.shelter_name.lower() == filter_.shelter_name.lower())
+
         return query
 
     def get_adverts(
@@ -21,13 +45,19 @@ class AdvertisementRepository:
             user_id: int,
             page: int,
             page_size: int,
-            filter: AdvertisementFilter = None,
+            filter_: AdvertisementFilter,
     ):
         query = (
             self.session.query(Advertisement)
-            .options(joinedload(Advertisement.user_posted), joinedload(Advertisement.pet_posted))
+            .options(
+                joinedload(Advertisement.user_posted),
+                joinedload(Advertisement.pet_posted),
+                joinedload(Advertisement.shelter)
+            )
+            .filter(Advertisement.deleted == False)
         )
-        query = self._filter_query(query)
+        if filter_:
+            query = self._filter_query(query, filter_)
 
         if not user_id:
             query = query.filter(Advertisement.category == AdvertisementCategory.LOST.value)
@@ -42,7 +72,11 @@ class AdvertisementRepository:
     def get_advert_by_id(self, advert_id: int):
         query = (
             self.session.query(Advertisement)
-            .options(joinedload(Advertisement.user_posted), joinedload(Advertisement.pet_posted), joinedload(Advertisement.shelter))
+            .options(
+                joinedload(Advertisement.user_posted),
+                joinedload(Advertisement.pet_posted),
+                joinedload(Advertisement.shelter)
+            )
             .filter(Advertisement.id == advert_id)
         )
 
