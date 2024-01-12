a0f89e2800ab50ae39a067f9f5e90679abfbbaf4
mirta <mirtakrajinovic17@gmail.com>
2024-01-12 15:17:04 +0100

    authorization shelters endpoint

--- backend-service/service/api/authorization/endpoints.py
+++ backend-service/service/api/authorization/endpoints.py
@@ -3,7 +3,7 @@ from typing import Annotated, List
 
 from fastapi import APIRouter, HTTPException, Response, Depends, Query
 from service.api.authorization.models import UserLogin, UserSignup, ShelterOutput
-from service.api.authorization.utils import generate_token, validate_signup, validate_token
+from service.api.authorization.utils import generate_token, validate_signup, validate_token, map_to_output_shelter
 from service.exceptions import InvalidLoginException
 from service.repository.authorization_repo import AuthorizationRepository
 
@@ -44,6 +44,7 @@ def sign_up(user_signup: UserSignup):
 
     return Response(status_code=201, content=json.dumps({"detail": "User successfully saved"}))
 
+
 @auth_router.get("/shelters")
 def get_shelters(
         user_id: Annotated[int, Depends(validate_token)],
@@ -52,3 +53,9 @@ def get_shelters(
 ) -> List[ShelterOutput]:
     repo = AuthorizationRepository()
     db_shelters = repo.get_shelters(page, page_size)
+    output_shelters = []
+
+    for db_shelter in db_shelters:
+        output_shelters.append(map_to_output_shelter(db_shelter))
+
+    return output_shelters
