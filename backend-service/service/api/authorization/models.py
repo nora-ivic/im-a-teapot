caab024540b24acda958a5e64d277bd7ed79216f
mirta <mirtakrajinovic17@gmail.com>
2024-01-12 12:14:44 +0100

    added ShelterOutput authorization model

--- backend-service/service/api/authorization/models.py
+++ backend-service/service/api/authorization/models.py
@@ -14,3 +14,10 @@ class UserSignup(UserLogin):
     shelter_name: str | None = Field(default=None, max_length=100)
     email: str = Field(pattern='^[A-Za-z0-9._+%-]+@[A-Za-z0-9.-]+[.][A-Za-z]+$')
     phone_number: str = Field(max_length=15)
+
+
+class ShelterOutput(BaseModel):
+    username: str = Field(min_length=5, max_length=50)
+    shelter_name: str = Field(max_length=100)
+    email: str = Field(pattern='^[A-Za-z0-9._+%-]+@[A-Za-z0-9.-]+[.][A-Za-z]+$')
+    phone_number: str = Field(max_length=15)
